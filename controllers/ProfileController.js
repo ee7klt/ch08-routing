54b167b1e1fa1235f34aa27f44b46d51b4e1354a
Manuel Schoebel <mail@manuelschoebel.de>
2014-10-13 01:24:13 +0200

    adding compliments

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -5,17 +5,32 @@ ProfileController = RouteController.extend({
     'profileDetailLeft': {to: 'left'}
   },
   waitOn: function(){
-    return Meteor.subscribe('profile', this.params._id);
+    return [
+      Meteor.subscribe('profile', this.params._id),
+      Meteor.subscribe('complimentsByUserId', this.params._id)
+    ]
   },
   data: function(){
-    return ProfilesCollection.findOne({_id: this.params._id});
+    var self = this;
+    console.log('DATA');
+    var data = {
+      profile: function(){
+        console.log('PROFILE');
+        return ProfilesCollection.findOne({_id: self.params._id});
+      },
+      compliments: function(){
+        console.log('COMPLIMENTS');
+        return ComplimentsCollection.find({userId: self.params._id});
+      }
+    };
+    return data;
   },
   stars: function(){
-    // guard the function in case data is undefined
-    if(!this.data()) { return; }
+    // guard the function in case data.profile is undefined
+    if(!this.data().profile()) { return; }
 
     // one star for each 50 views
-    stars = parseInt(this.data().views / 50);
+    stars = parseInt(this.data().profile().views / 50);
 
     // maximum of 5 stars, min 1 star
     stars = stars > 5 ? 5 : stars == 0 ? 1 : stars;
@@ -35,14 +50,13 @@ ProfileController.events({
     // the form should not perform a http post
     evt.preventDefault();
 
-    console.log(this.data(), tpl.data);
-
     var form = tpl.find('form');
     var compliment = tpl.find('textarea[name=compliment]').value;
 
     ComplimentsCollection.insert({
       text: compliment,
-      userId: this.data()._id
+      userId: this.data().profile()._id
     });
+    form.reset();
   }
 });
---
+++ server/publications/compliments.js
@@ -0,0 +1,12 @@
+Meteor.publish('complimentsByUserId', function(_id){
+  check(_id, String);
+
+
+  profiles = Meteor.wrapAsync(function(cb){
+    Meteor.setTimeout(function(){
+      cb(null, ComplimentsCollection.find({userId: _id}));
+    }, 1000);
+  })();
+
+  return profiles;
+});
