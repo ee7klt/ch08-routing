8465ae6e2f41f0ad135dc39d53aed77caaabb32d
Manuel Schoebel <mail@manuelschoebel.de>
2014-10-13 03:28:40 +0200

    server sider routes

--- router/routes.js
+++ router/routes.js
@@ -11,3 +11,29 @@ Router.plugin('bouncer', {
 Router.route('/', { name: 'home' });
 Router.route('/about', 'about', { name: 'about' });
 Router.route('/profiles/:_id', { name: 'profile' });
+
+// SERVER ROUTES
+
+// Middlewares
+// parses body data to json automatically
+Router.use(Router.bodyParser.json());
+
+Router.route('/server/say', function(){
+  var request = this.request;
+  var response = this.response;
+
+  response.end('You said: ' + request.query.message);
+}, {where: 'server'})
+
+// EXTERNAL SERVICES
+Router.route('/webhooks/instagram', {where: 'server'})
+  .get(function(){
+    // HTTP GET on the route /webhooks/instagram
+    this.request.end('Hi Instagram!');
+  })
+  .post(function(){
+    // HTTP POST on the route /webhooks/instagram
+  })
+  .put(function(){
+    // HTTP PUT on the route /webhooks/instagram
+  })
